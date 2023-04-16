d76b8785530aba740cd4ff663468ced2733d9d7a
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2023-04-16 05:29:40 +0200

    Update logoutController.js

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -2,12 +2,12 @@ const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
     // On client, also delete the accessToken
+
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(204); //No content
     const refreshToken = cookies.jwt;
 
     // Is refreshToken in db?
-    // const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
     const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
@@ -21,6 +21,6 @@ const handleLogout = async (req, res) => {
 
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
-};
+}
 
-module.exports = { handleLogout };
+module.exports = { handleLogout }
