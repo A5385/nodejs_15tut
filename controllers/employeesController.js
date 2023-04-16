dfff212f83b7dbb135aaeee4609a795160031be0
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2023-04-16 05:28:50 +0200

    Update employeesController.js

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -10,11 +10,13 @@ const createNewEmployee = async (req, res) => {
     if (!req?.body?.firstname || !req?.body?.lastname) {
         return res.status(400).json({ 'message': 'First and last names are required' });
     }
+
     try {
         const result = await Employee.create({
             firstname: req.body.firstname,
             lastname: req.body.lastname
         });
+
         res.status(201).json(result);
     } catch (err) {
         console.error(err);
@@ -27,9 +29,8 @@ const updateEmployee = async (req, res) => {
     }
 
     const employee = await Employee.findOne({ _id: req.body.id }).exec();
-
     if (!employee) {
-        return res.status(204).json({ "message": `No employee mateches ID ${req.body.id} ` });
+        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
     }
     if (req.body?.firstname) employee.firstname = req.body.firstname;
     if (req.body?.lastname) employee.lastname = req.body.lastname;
@@ -42,17 +43,18 @@ const deleteEmployee = async (req, res) => {
 
     const employee = await Employee.findOne({ _id: req.body.id }).exec();
     if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id} ` });
+        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
     }
-    const result = await employee.deleteOne({ _id: req.body.id });
+    const result = await employee.deleteOne(); //{ _id: req.body.id }
     res.json(result);
 };
 
 const getEmployee = async (req, res) => {
     if (!req?.params?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
+
+    const employee = await Employee.findOne({ _id: req.params.id }).exec();
     if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.params.id} ` });
+        return res.status(204).json({ "message": `No employee matches ID ${req.params.id}.` });
     }
     res.json(employee);
 };
