c7a3c52deed64b9c5a37c636e9cf052e5767baf4
Ahmed Khaled <116571050+A5385@users.noreply.github.com>
2023-04-16 05:30:20 +0200

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,12 +7,13 @@ const handleNewUser = async (req, res) => {
 
     // check for duplicate usernames in the db
     const duplicate = await User.findOne({ username: user }).exec();
-    if (duplicate) return res.sendStatus(409); //Conflict
+    if (duplicate) return res.sendStatus(409); //Conflict 
 
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        // create and store the new user
+
+        //create and store the new user
         const result = await User.create({
             "username": user,
             "password": hashedPwd
@@ -24,6 +25,6 @@ const handleNewUser = async (req, res) => {
     } catch (err) {
         res.status(500).json({ 'message': err.message });
     }
-};
+}
 
 module.exports = { handleNewUser };
